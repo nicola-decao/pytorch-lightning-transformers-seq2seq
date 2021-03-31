78835fb4a5132d08b352aaa04a52f6b5a56eed70
Nicola De Cao <nicola.decao@gmail.com>
2021-03-31 14:52:02 +0100

    Update train.py

--- scripts/train.py
+++ scripts/train.py
@@ -8,7 +8,7 @@ from pytorch_lightning.callbacks import LearningRateMonitor, ModelCheckpoint
 from pytorch_lightning.loggers import TensorBoardLogger
 from pytorch_lightning.utilities.seed import seed_everything
 
-from src.models.seq2seq import Seq2SeqModel
+from src.models.seq2seq import BartSeq2SeqModel
 
 if __name__ == "__main__":
     parser = ArgumentParser()
@@ -19,7 +19,7 @@ if __name__ == "__main__":
     parser.add_argument("--save_top_k", type=int, default=10)
     parser.add_argument("--seed", type=int, default=0)
 
-    parser = Seq2SeqModel.add_model_specific_args(parser)
+    parser = BartSeq2SeqModel.add_model_specific_args(parser)
     parser = Trainer.add_argparse_args(parser)
 
     args, _ = parser.parse_known_args()
@@ -43,6 +43,6 @@ if __name__ == "__main__":
 
     trainer = Trainer.from_argparse_args(args, logger=logger, callbacks=callbacks)
 
-    model = Seq2SeqModel(**vars(args))
+    model = BartSeq2SeqModel(**vars(args))
 
     trainer.fit(model)
